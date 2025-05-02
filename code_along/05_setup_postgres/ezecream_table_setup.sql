a42fd72fbdeeea66f5587db7c5e2bc5d03153068
AbdirahmanHassan47 <abdi1.hassan@icloud.com>
2025-05-02 16:49:01 +0200

    postgres

--- code_along/05_setup_postgres/ezecream_table_setup.sql
+++ code_along/05_setup_postgres/ezecream_table_setup.sql
@@ -1,8 +1,20 @@
 CREATE SCHEMA IF NOT EXISTS ezecream_05;
 
 
+SET search_path TO ezecream_05;
+
+CREATE TABLE IF NOT EXISTS
+    Customer (
+        customer_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        address VARCHAR(100),
+        email VARCHAR(255) UNIQUE 
+    );
 
 
 
 
+
+SELECT * FROM customer;
 
---
+++ code_along/05_setup_postgres/insert_costumer.sql
@@ -0,0 +1,6 @@
+SET search_path TO ezecream_05;
+
+
+INSERT INTO customer (first_name, last_name, address, email)
+VALUES
+    ('Ragnar', 'Lothbrok', '1 Valhalla Way', 'ragnar@kattegat.com');
